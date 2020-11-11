e646e511ae55863e9bafba3d8ab06dbac7e7b4cf
R3voA3 <robertseibel@outlook.de>
2020-11-11 17:51:57 +0100

    Update config.cpp

--- main_f_stringtable/config.cpp
+++ main_f_stringtable/config.cpp
@@ -1,38 +1,38 @@
 #include "cfgpatches.hpp"
 
 class Cfg3DEN {
-    class Notifications {
-		class 3DENStringtableViewer_preloading_start {
-			isWarning = 0;
-			text = "$STR_STRINGTABLE_3DEN_NOTIFICATION_PRELOADING_START";
-		};
-		class 3DENStringtableViewer_preloading_end {
-			isWarning = 0;
-			text = "$STR_STRINGTABLE_3DEN_NOTIFICATION_PRELOADING_END";
-		};
+   class Notifications {
+        class 3DENStringtableViewer_preloading_start {
+            isWarning = 0;
+            text = "$STR_STRINGTABLE_3DEN_NOTIFICATION_PRELOADING_START";
+        };
+        class 3DENStringtableViewer_preloading_end {
+            isWarning = 0;
+            text = "$STR_STRINGTABLE_3DEN_NOTIFICATION_PRELOADING_END";
+        };
     };
-class EventHandlers {
-	class 3DENStringtableViewer {
-		onTerrainNew="if ('Preferences' get3DENMissionAttribute '3DENStringtableViewer_EnablePreloading') then {['preload'] spawn STRINGTABLE_fnc_stringtable_viewer;}";
-	};
-};
-class Mission {
-	class Preferences {
-		class AttributeCategories {	
-			class Misc {
-				class Attributes {
-					class 3DENStringtableViewer_EnablePreloading
-					{
-					  displayName = $STR_STRINGTABLE_3DEN_PREFERENCES_ENABLEPRELOADING_DISPLAYNAME;
-       				          tooltip = $STR_STRINGTABLE_3DEN_PREFERENCES_ENABLEPRELOADING_TOOLTIP;
-					  property = "3DENStringtableViewer_EnablePreloading";
-					  control = "Checkbox";
-					  expression = "";
-					  defaultValue = "false";
-					};
-				};
-			};
-		};
-	};
-};				
+    class EventHandlers {
+        class 3DENStringtableViewer {
+            onTerrainNew="if ('Preferences' get3DENMissionAttribute '3DENStringtableViewer_EnablePreloading') then {['preload'] spawn STRINGTABLE_fnc_stringtable_viewer;}";
+        };
+    };
+    class Mission {
+        class Preferences {
+            class AttributeCategories {
+                class Misc {
+                    class Attributes {
+                        class 3DENStringtableViewer_EnablePreloading
+                        {
+                            displayName = $STR_STRINGTABLE_3DEN_PREFERENCES_ENABLEPRELOADING_DISPLAYNAME;
+                            tooltip = $STR_STRINGTABLE_3DEN_PREFERENCES_ENABLEPRELOADING_TOOLTIP;
+                            property = "3DENStringtableViewer_EnablePreloading";
+                            control = "Checkbox";
+                            expression = "";
+                            defaultValue = "false";
+                        };
+                    };
+                };
+            };
+        };
+    };
 };
